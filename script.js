dd2b1db802d31511d6bb1de95cc67653628ee44c
glenn15 <64649741+glenn15@users.noreply.github.com>
2021-06-01 22:02:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const btn1=document.getElementaryById("btn1");
 
-btn1.onclick = () => document.body.style.backgroundColor = "blue";
+btn1.onclick = () => button.body.style.backgroundColor = "blue";
